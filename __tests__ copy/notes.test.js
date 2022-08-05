49ee13d5b6c64b45a1d0992f98c7b82dcde057ed
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-08-05 17:13:47 -0400

    cleaning file paths

--- __tests__ copy/notes.test.js
+++ __tests__ copy/notes.test.js
@@ -2,25 +2,25 @@ const fs = require("fs");
 const {
   filterByQuery,
   findById,
-  createNewAnimal,
-  validateAnimal,
-} = require("../lib/animals.js");
-const { animals } = require("../data/animals");
+  createNewNote,
+  validateNote,
+} = require("../lib/notes.js");
+const { notes } = require("../data/notes");
 
 jest.mock('fs');
 
-test("creates an animal object", () => {
-  const animal = createNewAnimal(
+test("creates an note object", () => {
+  const note = createNewNote(
     { name: "Darlene", id: "jhgdja3ng2" },
-    animals
+    notes
   );
 
-  expect(animal.name).toBe("Darlene");
-  expect(animal.id).toBe("jhgdja3ng2");
+  expect(note.name).toBe("Darlene");
+  expect(note.id).toBe("jhgdja3ng2");
 });
 
 test("filters by query", () => {
-  const startingAnimals = [
+  const startingNotes = [
     {
       id: "3",
       name: "Erica",
@@ -36,13 +36,13 @@ test("filters by query", () => {
       personalityTraits: ["impish", "sassy", "brave"],
     }
   ];
-  const updateAnimals = filterByQuery({ species: "gorilla" }, startingAnimals);
+  const updateNotes = filterByQuery({ species: "gorilla" }, startingNotes);
 
-  expect(updateAnimals.length).toEqual(1);
+  expect(updateNotes.length).toEqual(1);
 });
 
 test("finds by id", () => {
-  const startingAnimals = [
+  const startingNotes = [
     {
       id: "3",
       name: "Erica",
@@ -59,13 +59,13 @@ test("finds by id", () => {
     },
   ];
 
-  const result = findById("3", startingAnimals);
+  const result = findById("3", startingNotes);
 
   expect(result.name).toBe("Erica");
 });
 
 test("validates personality traits", () => {
-  const animal = {
+  const note = {
     id: "3",
     name: "Erica",
     species: "gorilla",
@@ -73,15 +73,15 @@ test("validates personality traits", () => {
     personalityTraits: ["quirky", "rash"],
   };
 
-  const invalidAnimal = {
+  const invalidNote = {
     id: "3",
     name: "Erica",
     species: "gorilla",
     diet: "omnivore",
   };
 
-  const result = validateAnimal(animal);
-  const result2 = validateAnimal(invalidAnimal);
+  const result = validateNote(note);
+  const result2 = validateNote(invalidNote);
 
   expect(result).toBe(true);
   expect(result2).toBe(false);
