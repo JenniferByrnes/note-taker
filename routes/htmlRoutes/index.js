76507dd6caeee38ae69be59b6d19559bfcbf50d2
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-11-23 13:32:18 -0500

    vercel json add

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 
 router.get("/notes", (req, res) => {
   // res.sendFile(path.join(__dirname, "../../public/notes.html"));
-  res.sendFile('notes.html', { root: path.join(__dirname, 'public')})
+  res.sendFile('notes.html', { root: path.join(__dirname, '../../public')})
 });
 
 router.get("*", (req, res) => {
   // res.sendFile(path.join(__dirname, '../../public/index.html'));
-  res.sendFile('index.html', { root: path.join(__dirname, 'public')})
+  res.sendFile('index.html', { root: path.join(__dirname, '../../public')})
 });
 
 module.exports = router;
